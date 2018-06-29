521a2fe8fc9df9c776c0edc24862ae1dd39dc584
Sofia <cirquedusophie@gmail.com>
2018-06-28 21:34:22 -0300

    lista

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Lista from './Todolista/Lista';
+class App extends Component {
+  render() {
+    return (
+
+      <div className="App">
+        <Lista/>
+    
+      </div>
+  
+    );
+  }
+}
+
+export default App;
